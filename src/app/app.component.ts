7e230f667ba5503d5a9b49a362640067044880c2
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2023-09-20 01:04:53 -0400

    Template set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>Hello World!</h1>',
-  styles: ['h1 { color: red; }'],
+  templateUrl: './app.component.html',
+  styleUrls: ['app.component.css'],
 })
 export class AppComponent {}
