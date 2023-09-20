854a51e317589d8d9865b2ebb5feb10783d94f82
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2023-09-20 01:14:42 -0400

    Interpolation

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.css']
+  styleUrls: ['./books.component.css'],
 })
 export class BooksComponent {
+  name: string = 'Angular';
+  author: string = 'Youtube';
+  src: string = 'https://i.ytimg.com/vi/wkhClyW5uMc/maxresdefault.jpg';
 
+  name2: string = 'Angular Logo';
+  author2: string = 'Google';
+  src2: string =
+    'https://miro.medium.com/v2/resize:fit:1400/1*R1mfXLP9edcArZXwmGbGag.jpeg';
 }
