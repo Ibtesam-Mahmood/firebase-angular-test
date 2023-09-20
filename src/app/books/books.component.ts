0e7a9e3e7e86be1f8fb1955574e434ec4b3f0f94
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2023-09-20 01:23:44 -0400

    Event binding

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -16,4 +16,8 @@ export class BooksComponent {
   author2: string = 'Google';
   src2: string =
     'https://miro.medium.com/v2/resize:fit:1400/1*R1mfXLP9edcArZXwmGbGag.jpeg';
+
+  handleShowing(show: boolean): void {
+    this.isShowing = show;
+  }
 }
