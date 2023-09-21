e690541093dbedce51073c35b17dda9e0958ff8c
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2023-09-21 00:18:28 -0400

    services and depencency injection

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,34 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Book } from '../types/book';
+import { BooksService } from './books.service';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css'],
 })
-export class BooksComponent {
-  books: Book[] = [
-    {
-      name: 'Angular',
-      author: 'Youtube',
-      url: 'https://i.ytimg.com/vi/wkhClyW5uMc/maxresdefault.jpg',
-      amount: 20,
-    },
-    {
-      name: 'Angular Logo',
-      author: 'Google',
-      url: 'https://miro.medium.com/v2/resize:fit:1400/1*R1mfXLP9edcArZXwmGbGag.jpeg',
-      amount: 10,
-    },
-    {
-      name: 'Photography',
-      author: 'Unsplash',
-      url: 'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80',
-      amount: 30,
-    },
-  ];
-
+export class BooksComponent implements OnInit {
+  books: Book[] = [];
   cart: Book[] = [];
+
+  // Dependency injection of BooksService fromnthe Angular Service Container
+  constructor(private bookService: BooksService) {}
+
+  ngOnInit(): void {
+    this.books = this.bookService.getBooks();
+  }
 
   addToCart(book: Book) {
     this.cart.push(book);
---
+++ src/app/books/books.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Book } from '../types/book';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BooksService {
+  constructor() {}
+
+  getBooks(): Book[] {
+    return [
+      {
+        name: 'Angular',
+        author: 'Youtube',
+        url: 'https://i.ytimg.com/vi/wkhClyW5uMc/maxresdefault.jpg',
+        amount: 20,
+      },
+      {
+        name: 'Angular Logo',
+        author: 'Google',
+        url: 'https://miro.medium.com/v2/resize:fit:1400/1*R1mfXLP9edcArZXwmGbGag.jpeg',
+        amount: 10,
+      },
+      {
+        name: 'Photography',
+        author: 'Unsplash',
+        url: 'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80',
+        amount: 30,
+      },
+    ];
+  }
+}
